17eda91a87e10b16cfa466339c5dfd7a83b37a57
324235258 <noalow1@gmail.com>
2025-11-19 21:41:16 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,5 +1,6 @@
 def split_before_each_uppercases(formula):
     split_formula = []
+    start = 0
     for end, ch in enumerate(formula[1:], start=1):
         if ch.isupper():
             split_formula.append(formula[start:end])
@@ -7,9 +8,9 @@ def split_before_each_uppercases(formula):
     split_formula.append(formula[start:])
     return split_formula
 
+
 def split_at_first_digit(formula):
     for i, ch in enumerate(formula):
         if ch.isdigit():
             return formula[:i], int(formula[i:])
     return formula, 1
-
